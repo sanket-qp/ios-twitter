7bed7cfb0c22965e632b4ef779f4c2a5870a835e
Sanket Patel <sanket@qpme.com>
2014-09-26 11:39:47 -0700

    initial commit

---
+++ ios-twitter/LoginViewController.swift
@@ -0,0 +1,43 @@
+//
+//  LoginViewController.swift
+//  ios-twitter
+//
+//  Created by sanket patel on 9/25/14.
+//  Copyright (c) 2014 sanket patel. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController, UITextFieldDelegate {
+
+    
+    @IBOutlet weak var webView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        //passwordTextField.returnKeyType = UIReturnKeyType.Done
+        //passwordTextField.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ios-twitter/Tweet.swift
@@ -0,0 +1,14 @@
+//
+//  Tweet.swift
+//  ios-twitter
+//
+//  Created by sanket patel on 9/26/14.
+//  Copyright (c) 2014 sanket patel. All rights reserved.
+//
+
+import Foundation
+class Tweet {
+
+
+
+}
